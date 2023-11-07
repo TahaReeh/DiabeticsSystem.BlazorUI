6154ef1e8c40a1f26f37e8ab93887101c7595a22
Taha Reeh <tahareehg@gmail.com>
2023-11-08 00:02:27 +0200

    implemented clean arcticture + created table component

---
+++ Core/SharedComponanats/TableWidget.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DiabeticsSystem.BlazorUI.Core.SharedComponanats
+{
+    public partial class TableWidget<TItem>
+    {
+        [Parameter]
+        public RenderFragment HeaderTemplate { get; set; }
+
+        [Parameter]
+        public RenderFragment<TItem> RowTemplate { get; set; }
+
+        [Parameter]
+        public IReadOnlyList<TItem> Items { get; set; }
+
+        [Parameter]
+        public string title { get; set; }
+    }
+}
---
+++ Features/Product/Presentation/Logic/ProductBase.cs
@@ -0,0 +1,19 @@
+﻿using DiabeticsSystem.BlazorUI.Features.Product.Domain.Entitiy;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace DiabeticsSystem.BlazorUI.Features.Product.Presentation.Logic
+{
+    public class ProductBase : ComponentBase
+    {
+        [Inject]
+        private HttpClient? Http { get; set; }
+
+        public List<ProductListVM>? productListVMs;
+
+        protected override async Task OnInitializedAsync()
+        {
+            productListVMs = await Http!.GetFromJsonAsync<List<ProductListVM>>("api/Product/GetAllProducts");
+        }
+    }
+}
